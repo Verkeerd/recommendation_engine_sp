93165b1aca29142dae8838b6e7fb13938748616f
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-25 21:46:54 +0100

    data is correctly processed

--- data_transfer_profiles.py
+++ data_transfer_profiles.py
@@ -4,14 +4,30 @@ import sql_connection as sql_c
 
 def create_profile_query(profile):
     """"""
-    sql_query = 'INSERT INTO profiles (profile__id, created, latest_order, first_order, latest_activity) ' \
-                'VALUES (%s, %s, %s, %s, %s)'
+    sql_query_qp = 'INSERT INTO profiles (profile__id)'
+    sql_query_vp = ' VALUES (%s)'
 
-    wanted_values = (str(profile['_id']),
-                     profile['sm']['created'],
-                     profile['order']['latest'],
-                     profile['order']['first'],
-                     profile['latest_activity'])
+    wanted_values = (str(profile['_id']),)
+    try:
+        wanted_values += (profile['sm']['created'],)
+        sql_query_qp = sql_query_qp[:-1] + ', created)'
+        sql_query_vp = sql_query_vp[:-1] + ', %s)'
+    except KeyError:
+        pass
+    try:
+        wanted_values += (profile['order']['latest'], profile['order']['first'])
+        sql_query_qp = sql_query_qp[:-1] + ', latest_order, first_order)'
+        sql_query_vp = sql_query_vp[:-1] + ', %s, %s)'
+    except KeyError:
+        pass
+    try:
+        wanted_values += (profile['latest_activity'],)
+        sql_query_qp = sql_query_qp[:-1] + ', latest_activity)'
+        sql_query_vp = sql_query_vp[:-1] + ', %s)'
+    except KeyError:
+        pass
+
+    sql_query = sql_query_qp + sql_query_vp
     print(sql_query, wanted_values)
     return sql_query, wanted_values
 
@@ -19,19 +35,20 @@ def create_profile_query(profile):
 def create_buid_query(profile_id, buid):
     """"""
     sql_query = """INSERT INTO buid (buid, profile__id) 
-    VALUES (%s, %s)"""
+    VALUES (%s, %s) ON CONFLICT ON CONSTRAINT c_b_pk DO NOTHING;"""
 
     return sql_query, (str(buid), str(profile_id))
 
 
-def create_recommendation_query(profile):
+def create_recommendation_query(profile, recommendation_id):
     """"""
-    sql_query = """INSERT INTO recommendations (profile__id, recommendation_time, segment, last_visit, total_pageview_count, 
-    total_viewed_count) 
-    VALUES (%s, %s, %s, %s, %s, %s)"""
+    sql_query = """INSERT INTO recommendations (recommendation_id, profile__id, recommendation_time, segment, 
+    last_visit, total_pageview_count, total_viewed_count) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s);"""
 
     profile_rec = profile['recommendations']
-    wanted_values = (str(profile['_id']),
+    wanted_values = (recommendation_id,
+                     str(profile['_id']),
                      profile_rec['timestamp'],
                      profile_rec['segment'],
                      profile_rec['latest_visit'],
@@ -43,8 +60,15 @@ def create_recommendation_query(profile):
 
 def create_recommendation_product_query(recommendation_id, product__id, recommendation_type):
     """"""
-    sql_query = """INSERT INTO recommendation_products (recommendation_id, product__id, recommendation_type) 
-        VALUES (%s, %s, %s) ON CONFLICT CONSTRAINT c_rp_fk_pid DO NOTHING"""
+    sql_query = """INSERT INTO recommendation_products (recommendation_id, product__id, recommendation_type)
+                   SELECT rp.*
+                   FROM  (VALUES (%s, %s, %s)) rp(recommendation_id, product__id, recommendation_type)
+                
+                   WHERE  EXISTS (
+                   SELECT FROM products p                                 
+                   WHERE p.product__id = rp.product__id 
+                   FOR    SHARE                                                -- weakest lock
+                   );"""
 
     return sql_query, (recommendation_id, str(product__id), recommendation_type)
 
@@ -52,10 +76,19 @@ def create_recommendation_product_query(recommendation_id, product__id, recommen
 def upload_profile(profile):
     """"""
     sql_connection, sql_cursor = sql_c.connect()
+    print(profile['_id'])
+    # skips profiles without associated buids.
+    try:
+        buids = profile['buids']
+        if len(buids) == 0:
+            return None
+    except KeyError:
+        return None
+
     # creates entry in profile table
     profile_query, profile_values = create_profile_query(profile)
     sql_cursor.execute(profile_query, profile_values)
-    buids = profile['buids']
+
     # creates entries in buid table
     for buid in buids:
         buid_query, buid_values = create_buid_query(profile['_id'], buid)
@@ -63,9 +96,12 @@ def upload_profile(profile):
     # creates entries in recommendation table and recommendation_product table if data is available.
     try:
         recommendation_data = profile['recommendations']
-        recommendation_query, reccomendation_values = create_recommendation_query(profile)
-        sql_cursor.execute(recommendation_query, reccomendation_values)
+        # generates recommendation_id with the sql_sequence
+        sql_cursor.execute("""SELECT nextval('recommendation_seq')""")
         recommendation_id = sql_cursor.fetchone()[0]
+
+        recommendation_query, recommendation_values = create_recommendation_query(profile, recommendation_id)
+        sql_cursor.execute(recommendation_query, recommendation_values)
         for viewed_item in recommendation_data['viewed_before']:
             rp_query, rp_values = create_recommendation_product_query(recommendation_id, viewed_item, 'viewed')
             sql_cursor.execute(rp_query, rp_values)
@@ -87,10 +123,18 @@ def upload_all_profiles():
 
     for profile in list(profile_collection.find()):
         print(profile['_id'])
+        # skips profiles without associated buids.
+        try:
+            buids = profile['buids']
+            if len(buids) == 0:
+                continue
+        except KeyError:
+            continue
+
         # creates entry in profile table
         profile_query, profile_values = create_profile_query(profile)
         sql_cursor.execute(profile_query, profile_values)
-        buids = profile['buids']
+
         # creates entries in buid table
         for buid in buids:
             buid_query, buid_values = create_buid_query(profile['_id'], buid)
@@ -98,9 +142,12 @@ def upload_all_profiles():
       # creates entries in recommendation table and recommendation_product table if data is available.
         try:
             recommendation_data = profile['recommendations']
-            recommendation_query, reccomendation_values = create_recommendation_query(profile)
-            sql_cursor.execute(recommendation_query, reccomendation_values)
+            # generates recommendation_id with the sql_sequence
+            sql_cursor.execute("""SELECT nextval('recommendation_seq')""")
             recommendation_id = sql_cursor.fetchone()[0]
+
+            recommendation_query, recommendation_values = create_recommendation_query(profile, recommendation_id)
+            sql_cursor.execute(recommendation_query, recommendation_values)
             for viewed_item in recommendation_data['viewed_before']:
                 rp_query, rp_values = create_recommendation_product_query(recommendation_id, viewed_item, 'viewed')
                 sql_cursor.execute(rp_query, rp_values)
