280399691069df759eef6873bd0dae3864215a60
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-27 00:43:32 +0100

    improved readability of sql_query simplified sql_query creation (fixed bug) added commit

--- data_transfer_sessions.py
+++ data_transfer_sessions.py
@@ -2,7 +2,7 @@ import mongo_connection as mdb_c
 import sql_connection as sql_c
 
 
-def create_session_query(session):
+def create_session_query(session, buids):
     """
     Takes session_data (dict) as input.
     Selects the following data from the session:
@@ -14,11 +14,13 @@ def create_session_query(session):
     - segment
     Creates an sql query to insert this data in the sessions table and returns this query.
     """
-    sql_query_qp = """INSERT INTO sessions (session__id, buid, session_start, session_end, has_sale)"""
-    sql_query_vp = """ VALUES (%s, %s, %s, %s, %s)"""
+    sql_query_format = """INSERT INTO sessions ({}) VALUES ({});"""
+    fields = 'session__id, buid, session_start, session_end, has_sale'
 
     try:
         buid = session['buid'][0]
+        if buid not in buids:
+            buid = '0'
     except KeyError:
         buid = '0'
 
@@ -30,12 +32,10 @@ def create_session_query(session):
 
     try:
         wanted_values += (session['segment'],)
-        sql_query_qp = sql_query_qp[:-1] + ', segment)'
-        sql_query_vp = sql_query_vp[:-1] + ', %s)'
+        fields += ', segment'
     except KeyError:
         pass
-
-    sql_query = sql_query_qp + sql_query_vp
+    sql_query = sql_query_format.format(fields, ('%s, ' * len(fields.split()))[:-2])
 
     return sql_query, wanted_values
 
@@ -57,16 +57,14 @@ def create_event_query(event, session_id, previous_events):
     Creates an sql query to insert this data (with the other input data) in the events table.
     Returns this query.
     """
-    sql_query_qp = """INSERT INTO events (session__id, previous_events, event_time, event_source, event_action, 
-    page_type)"""
-    sql_query_vp = """ VALUES (%s, %s, %s, %s, %s, %s)"""
-
+    sql_query_format = """INSERT INTO events ({}) VALUES ({})"""
+    fields = 'session__id, previous_events, event_time, event_source, event_action, page_type'
     wanted_values = (session_id,
                      previous_events,
                      event['t'],
                      event['source'],
                      event['action'],
-                     event['pagetype'],)
+                     event['pagetype'])
     try:
         wanted_values += (event['product'],
                      event['time_on_page'],
@@ -74,13 +72,11 @@ def create_event_query(event, session_id, previous_events):
                      event['elements_clicked'],
                      event['scrolls_down'],
                      event['scrolls_up'])
-        sql_query_qp = sql_query_qp[:-1] + \
-                       'product, time_on_page, click_count, elements_clicked, scrolls_down, scrolls_up)'
-        sql_query_vp = sql_query_vp[:-1] + '%s, %s, %s, %s, %s, %s)'
+        fields += ', product, time_on_page, click_count, elements_clicked, scrolls_down, scrolls_up'
     except KeyError:
         pass
 
-    sql_query = sql_query_qp + sql_query_vp
+    sql_query = sql_query_format.format(fields, ('%s, ' * (len(fields.split())))[:-2])
 
     return sql_query, wanted_values
 
@@ -90,7 +86,16 @@ def create_ordered_product_query(session_id, product_id, product_count):
     Takes a session_id (str) and, product__id (str) and a product_count (int) as input.
     Creates an sql query to insert this data in the ordered_products table and returns this query.
     """
-    sql_query = """INSERT INTO ordered_products (session__id, product__id, total) VALUES (%s, %s, %s)"""
+    # source where exists/for share clause: Erwin Brandstetter
+    # https://dba.stackexchange.com/questions/252875/how-to-make-on-conflict-work-for-compound-foreign-key-columns
+    sql_query = """INSERT INTO ordered_products (session__id, product__id, total) 
+                   SELECT op.*
+                   FROM  (VALUES (%s, %s, %s)) op(session__id, product__id, total)
+                
+                   WHERE  EXISTS (
+                   SELECT FROM products p                                 
+                   WHERE p.product__id = op.product__id 
+                   FOR    SHARE);"""
 
     return sql_query, (session_id, product_id, product_count)
 
@@ -120,7 +125,7 @@ def create_preference_query():
     return sql_query
 
 
-def upload_session(sql_cursor, session):
+def upload_session(sql_cursor, session, buids):
     """
     Takes an active sql_cursor and profile-data (dict) as input.
     Creates several sql queries to upload the profile data to the following sql tables:
@@ -137,7 +142,7 @@ def upload_session(sql_cursor, session):
     except KeyError:
         return None
     # creates entry for session
-    session_query, session_values = create_session_query(session)
+    session_query, session_values = create_session_query(session, buids)
     sql_cursor.execute(session_query, session_values)
     session_id = session['_id']
     # creates entries for events
@@ -153,7 +158,6 @@ def upload_session(sql_cursor, session):
         preferences = session['preferences']
         for category, entries in list(preferences.items()):
             entry_name, view_count = list(entries.items())[0]
-            print(session_id, category, entry_name, view_count)
             sql_cursor.execute(create_preference_query(), (session_id, category, entry_name, view_count['views']))
     except KeyError:
         pass
@@ -165,10 +169,13 @@ def upload_all_sessions():
     session_collection = database.sessions
     sql_connection, sql_cursor = sql_c.connect()
 
+    sql_cursor.execute("""SELECT buid FROM buid""")
+    buids = [data[0] for data in sql_cursor.fetchall()]
     for session in session_collection.find():
         session = dict(session)
-        upload_session(sql_cursor, session)
+        upload_session(sql_cursor, session, buids)
 
+    sql_connection.commit()
     sql_c.disconnect(sql_connection, sql_cursor)
 
 
