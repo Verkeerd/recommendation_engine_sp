5c3f61e6739ffaf3c13f6342aa12e7caa917b2e6
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-29 14:50:01 +0200

    profiles can be queried for -recommended products - profile__ids

--- load_data_sql.py
+++ load_data_sql.py
@@ -1,6 +1,7 @@
 import sql_connection as sql_c
 
 
+# products
 def all_product_prices():
     """Fetches the price of all products in the sql web-shop database. Returns these values (list) [(int)]."""
     sql_query = """SELECT selling_price FROM products"""
@@ -31,5 +32,61 @@ def name_price_products():
     return products
 
 
+# profiles
+def profile_ids():
+    """"""
+    connection, cursor = sql_c.connect()
+
+    cursor.execute("""SELECT profile__id FROM profiles""")
+    profiles = cursor.fetchall()
+
+    sql_c.disconnect(connection, cursor)
+
+    return profiles
+
+
+def all_users_profile_type_query():
+    """"""
+    return "SELECT profile__id FROM recommendations WHERE segment = '{}'"
+
+
+def fetch_profile_type(profile_id):
+    """"""
+    connection, cursor = sql_c.connect()
+    cursor.execute("""SELECT segment FROM recommendations WHERE profile__id = '{}'""".format(profile_id))
+    try:
+        return cursor.fetchone()
+    except:
+        cursor.execute("""SELECT segment FROM preferences WHERE session__id = (
+    SELECT session__id FROM sessions WHERE buid = (
+    SELECT buid FROM buid WHERE profile__id = '{}'))""".format(profile_id))
+        try:
+            return cursor.fetchall()
+        except:
+            return None
+
+
+# recommended products
+def recommended_products_profile_query():
+    """"""
+    # returns all products that are/have been recommended to this profile.
+    # TODO: Filter bought (non-repeat) products
+    # TODO: Filter often recommended but never bought products. Cap it? → depending on how this pans out in the final
+    #  design, probably restructure the database
+    return "SELECT product__id FROM recommendation_products WHERE recommendation_id =" \
+           "(SELECT recommendation_id FROM recommendations WHERE profile__id = '{}')"
+
+
+
+def recommended_products_several_profiles_query():
+    """"""
+    # returns all products that are/have been recommended to this profile.
+    # TODO: Filter bought (non-repeat) products
+    # TODO: Filter often recommended but never bought products. Cap it? → depending on how this pans out in the final
+    #  design, probably restructure the database
+    return "SELECT product__id FROM recommendation_products WHERE recommendation_id IN (" \
+           "(SELECT recommendation_id FROM recommendations WHERE profile__id IN ({})))"
+
+
 if __name__ == '__main__':
-    print(name_price_products())
+    pass
