e5fb42eecf44d097d3e4441b09c26d088bcaaf11
verkeerd <susan.bruggeling@student.hu.nl>
2022-03-30 14:33:44 +0200

    gathers all values and execute many sql inserts in one go

--- data_transfer_products.py
+++ data_transfer_products.py
@@ -1,74 +1,57 @@
 import mongo_connection as mdb_c
 import sql_connection as sql_c
-
-# TODO: make 1 big sql statement and execute it in one go.
-
-
-def secure_value(value):
-    """Turns empty strings and integers (0 and "0") into None. Returns all other values unchanged."""
-    if value == 0:
-        return None
-    if value == '0':
-        return None
-    return value
+import transfer_functions as shared
 
 
-def create_product_query(product):
+def create_product_query():
     """
     Takes products data (dict) as input.
     Selects desired data. Creates an sql query to insert this data in the recommendation_products table.
     Returns this query.
     """
-    sql_query_format = 'INSERT INTO  products ({}) VALUES ({})'
+    return """
 
-    fields_to_insert = ''
+    INSERT INTO  products (product__id, product_name, brand, category, color, fast_mover, flavor, gender, 
+    herhaalaankopen, recommendable, sub_category, sub_sub_category, sub_sub_sub_category, selling_price, availability, 
+    discount, doelgroep, eenheid, factor, folder_actief, gebruik, geschikt_voor, geursoort, huidconditie, huidtype, 
+    huidtypegezicht, inhoud, klacht, kleur, leeftijd, mid, online_only, serie, shopcart_promo_item, 
+    shopcart_promo_price, soort, soort_mondverzorging, sterkte, product_type, typehaarkleuring, typetandenbostel, 
+    variant, waterproof, weekdeal) 
 
-    # field names that are different between mongodb and sql.
-    bad_values = ['_id', 'name', 'type']
-    # wanted subdirectories
-    cat_keys = ['price', 'properties']
-    # wanted fields
-    wanted_keys = ['_id', 'brand', 'category', 'color', 'fast_mover', 'flavor', 'gender', 'herhaalaankopen',
-            'name', 'selling_price', 'availability', 'discount', 'doelgroep', 'eenheid', 'factor', 'folder_actief',
-            'gebruik', 'geschikt_voor', 'geursoort', 'huidconditie', 'huidtype', 'huidtypegezicht', 'inhoud', 'klacht',
-            'kleur', 'leeftijd', 'mid', 'online_only', 'serie', 'shopcart_promo_item', 'shopcart_promo_price', 'soort',
-            'soort_mondverzorging', 'sterkte', 'type', 'typehaarkleuring', 'typetandenbostel', 'variant', 'waterproof',
-            'weekdeal', 'recommendable', 'sub_category', 'sub_sub_category', 'sub_sub_sub_category']
+    VALUES ({} %s);
+    """.format('%s,' * 43)
 
-    wanted_values = ()
 
-    for key, value in product.items():
-        # check if the field is a wanted subdirectory
-        if key in cat_keys:
-            # checks all keys and values in the subdirectory
-            for sub_key, sub_value in value.items():
-                if sub_key in wanted_keys:
-                    # filters out the discounted price in the price subdirectory. Discount in 'properties' is recorded.
-                    if not (key == 'price' and sub_key == 'discount'):
-                        # changes the column name when the sql_field is named differently than the mdb field
-                        if sub_key in bad_values:
-                            fields_to_insert += 'product_{}, '.format(sub_key)
-                        else:
-                            fields_to_insert += '{}, '.format(sub_key)
-                        wanted_values += (secure_value(sub_value),)
-        elif key in wanted_keys:
-            # changes the column name when the sql_field is named differently than the mdb field
-            if key in bad_values:
-                fields_to_insert += 'product_{}, '.format(key)
-            else:
-                fields_to_insert += '{}, '.format(key)
-            wanted_values += (secure_value(value),)
+def get_product_values(product):
+    """"""
+    result = tuple()
+    wanted_fields = ['_id', 'name', 'brand', 'category', 'color', 'fast_mover', 'flavor', 'gender', 'herhaalaankopen',
+                     'recommendable', 'sub_category', 'sub_sub_category', 'sub_sub_sub_category']
 
-    sql_query = sql_query_format.format(fields_to_insert[:-2], ('%s, ' * len(wanted_values))[:-2])
+    wanted_price_fields = ['selling_price']
 
-    return sql_query, wanted_values
+    wanted_property_fields = ['availability', 'discount', 'doelgroep', 'eenheid', 'factor', 'folder_actief', 'gebruik',
+                              'geschikt_voor', 'geursoort', 'huidconditie', 'huidtype', 'huidtypegezicht', 'inhoud',
+                              'klacht', 'kleur', 'leeftijd', 'mid', 'online_only', 'serie', 'shopcart_promo_item',
+                              'shopcart_promo_price', 'soort', 'soort_mondverzorging', 'sterkte', 'type',
+                              'typehaarkleuring', 'typetandenbostel', 'variant', 'waterproof', 'weekdeal']
+
+    for field in wanted_fields:
+        result += (shared.secure_dict_item(product, field),)
+    for field in wanted_price_fields:
+        result += (shared.secure_dict_item_double(product, 'price', field),)
+    for field in wanted_property_fields:
+        result += (shared.secure_dict_item_double(product, 'properties', field),)
+
+    return result
 
 
 def upload_product(product):
     """Takes information about a product (dict) as input. Uploads the product to the local sql database."""
     connection, cursor = sql_c.connect()
-    sql_query = create_product_query(product)
-    cursor.execute(sql_query)
+    sql_query = create_product_query()
+    values_to_insert = get_product_values(product)
+    cursor.execute(sql_query, values_to_insert)
     connection.commit()
     sql_c.disconnect(connection, cursor)
 
@@ -78,10 +61,12 @@ def upload_all_products():
     database = mdb_c.connect_mdb()
     products_collection = database.products
     sql_connection, sql_cursor = sql_c.connect()
+    sql_query = create_product_query()
+    values_to_insert = []
     for product in products_collection.find():
-        product = dict(product)
-        sql_query, format_values = create_product_query(product)
-        sql_cursor.execute(sql_query, format_values)
+        values_to_insert.append(get_product_values(product))
+
+    sql_cursor.executemany(sql_query, values_to_insert)
 
     sql_connection.commit()
     sql_c.disconnect(sql_connection, sql_cursor)
